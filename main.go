73a921cb0d8db743d0ddf03219fa76305024e26e
Daniel <danielh839@gmail.com>
2022-05-29 19:06:24 -0400

    renamed methods

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"goba2/backend"
 	"net/http"
 )
 
 func main() {
-	router := backend.NewServer().Routes()
-	fmt.Println("server started...")
-	http.ListenAndServe("localhost:3000", router)
+	router := backend.New().Router()
+	http.ListenAndServe(":3000", router)
 }
